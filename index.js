c4e6a57260af2c49953141a8f5089a94209a3295
bsh-ko <ssuh3872@gmail.com>
2024-09-23 17:18:04 +0900

    삭제 버튼 동작

--- index.js
+++ index.js
@@ -1,46 +1,55 @@
-"use strict"
+"use strict";
 
-let todoArray = []
-
-let inputNode = document.getElementById('todoList')
-let resultNode = document.getElementById('todoList_result')
+let todoArray = [];
+let inputNode = document.getElementById('todoList');
+let resultNode = document.getElementById('todoList_result');
+let childWindow; // 자식 창을 위한 변수
+let currentIndex; // 현재 수정 중인 항목의 인덱스
 
 // todoList를 출력해주는 함수
-function printResult(array) {
-    let result = ''
-    array.forEach((value, index) => {
-        result += `
+function printResult() {
+    resultNode.innerHTML = ''; // 기존 결과를 비우기
+    todoArray.forEach((value, index) => {
+        resultNode.innerHTML += `
         <div id='checkbox-layout'>
             <li class='todo-checkbox' data-index='${index}'>${value}</li> 
             <div id='btn_layout'>
-                <button class='modify_btn' onclick='goMemo()'>수정</button>
-                <button class='delete_btn' onclick='deleteList()'>삭제</button>
+                <button class='modify_btn' onclick='goMemo(${index})'>수정</button>
+                <button class='delete_btn' onclick='deleteList(${index})'>삭제</button>
             </div>
-        </div>`
-    })
-    resultNode.innerHTML = result
+        </div>`;
+    });
 }
 
-function goMemo() {
-    window.open('memo.html', '_blank', 'left=100, top=100, width=500, height=500')
+function goMemo(index) {
+    currentIndex = index; // 수정할 항목의 인덱스를 저장
+    if (childWindow) {
+        childWindow.close(); // 이미 열린 자식 창이 있다면 닫기
+    }
+    
+    childWindow = window.open('memo.html', '_blank', 'left=100, top=100, width=500, height=500');
+    childWindow.onload = function() {
+        const titleElement = childWindow.document.getElementById('title'); // memo.html에서 타이틀을 입력할 요소
+        titleElement.innerText = todoArray[index]; // 수정할 항목의 텍스트
+    };
 }
 
 function addList() {
-    let todo = inputNode.value
-    todoArray.push(todo)
-    printResult(todoArray)
-    inputNode.value = ''  // 입력 필드를 비워줌
+    let todo = inputNode.value.trim();
+    if (todo) {
+        todoArray.push(todo);
+        printResult();
+        inputNode.value = ''; // 입력 필드 비우기
+    }
 }
 
-function deleteList() {
-    // 체크된 항목들만 삭제 (뒤에서부터 삭제)
-    let checkboxes = document.querySelectorAll('.todo-checkbox')
-    for (let i = checkboxes.length - 1; i >= 0; i--) {
-        if (checkboxes[i].checked) {
-            let index = checkboxes[i].getAttribute('data-index')
-            todoArray.splice(index, 1)  // 해당 인덱스에서 항목 제거
-        }
-    }
-    // 삭제 후 다시 리스트를 출력
-    printResult(todoArray)
+function deleteList(index) {
+    todoArray.splice(index, 1); // 해당 인덱스에서 항목 제거
+    printResult(); // 삭제 후 다시 리스트를 출력
+}
+
+// memo.html에서 호출될 함수
+function updateTodo(newTodo) {
+    todoArray[currentIndex] = newTodo; // 수정한 항목으로 업데이트
+    printResult(); // 수정 후 리스트를 다시 출력
 }
--- memo.js
+++ memo.js
@@ -1,9 +1,20 @@
 "use strict";
 
+window.onload = function() {
+    // 부모 창에서 전달된 데이터 사용
+    if (mainData) {
+        document.querySelector('.title').textContent = mainData;  // 제목에 표시
+    }
+};
+
 function cancel_btn() {
     window.close()
 }
 
+// 예시: 수정 완료 버튼 클릭 시
 function ok_btn() {
-    
+    console.log('수정 완료:', document.querySelector('.title').textContent);
+    const title = document.querySelector('.title').innerText; // 수정된 내용 가져오기   
+    opener.updateTodo(title); // 부모 창의 updateTodo 호출
+    alert('수정 완료되었습니다!')
 }
